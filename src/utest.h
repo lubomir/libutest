05f60c3cb1bd66fe33ee6621e2cb199b5c30d6f6
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2013-04-13 15:39:17 +0200

    Add even more documentation

--- src/utest.h
+++ src/utest.h
@@ -95,7 +95,9 @@ void ut_register_callback(void (*cb)(void), const char *suite, int type);
 /** @endcond */
 
 /**
- * Define a new test.
+ * Define a new test in global file suite.
+ *
+ * The test name should be an identifier such as for a function or a variable.
  *
  * @param tname name of the new test
  */
@@ -105,6 +107,8 @@ void ut_register_callback(void (*cb)(void), const char *suite, int type);
 /**
  * Define new test in a suite.
  *
+ * Both arguments should be identifiers such as for a function or a variable.
+ *
  * @param suite name of the suite
  * @param tname test name
  */
@@ -112,56 +116,94 @@ void ut_register_callback(void (*cb)(void), const char *suite, int type);
     _ut_UT_SUITE_TEST(suite, STRINGIFY(suite), tname)
 
 /**
- * Fail the test unless exp evaluates to true.
+ * Fail the test unless `exp` evaluates to true.
+ *
+ * This is the simplest possible assertion. You can use it to perform any kind
+ * of test you need. However, using some more specialized assertion, you can
+ * get better error message if the assertion fails.
+ *
+ * @param exp   expression that must evaluate to `TRUE`
  */
 #define ut_assert(exp) ut_assert_func(exp, STRINGIFY(exp))
 
 /**
+ * Fail the test unless `exp` evaluates to `NULL`.
+ *
+ * @param exp   expression that must evaluate to `NULL`
  */
 #define ut_assert_null(exp)                                                 \
     ut_assert_func(exp == NULL, INBOLD(STRINGIFY(exp)) " is not NULL")
 
 /**
+ * Fail the test if expression evaluates to `NULL`.
+ *
+ * @param exp   expression that must evaluate to anything but `NULL`
  */
 #define ut_assert_not_null(exp)                                             \
     ut_assert_func(exp != NULL, INBOLD(STRINGIFY(exp)) " is NULL")
 
 /**
+ * Fail the test unless two integer values are the same.
+ *
+ * @param expected  expected value in the second parameter
+ * @param actual    expression that evaluates to integer value
  */
-#define ut_assert_equal_int(expected, actual)                                  \
+#define ut_assert_equal_int(expected, actual)                               \
     ut_assert_eq_with_type(int, UT_DIRECT_EQ, "%d", expected, actual)
 
 /**
+ * Fail the test unless two unsigned integer values are the same.
+ *
+ * @param expected  expected value in the second parameter
+ * @param actual    expression that evaluates to unsigned integer value
  */
-#define ut_assert_equal_uint(expected, actual)                                 \
+#define ut_assert_equal_uint(expected, actual)                              \
     ut_assert_eq_with_type(unsigned int, UT_DIRECT_EQ, "%u", expected, actual)
 
 /**
+ * Fail the test unless two strings are non-`NULL` and have the same contents.
+ *
+ * @param expected  expected value of the string
+ * @param actual    expression that evaluates to a pointer to `char`
  */
 #define ut_assert_equal_string(expected, actual)                            \
     ut_assert_eq_with_type(char *, UT_STR_EQ, "%s", expected, actual)
 
-
 /**
- */
-#define UT_SUITE_SETUP(suitename)                                           \
-    _ut_UT_SETUP(suitename, STRINGIFY(suitename))
-
-/**
+ * Add a setup function to the default file suite.
+ *
+ * This code will be run before each test in this file that is not explicitly
+ * added to a named test suite.
  */
 #define UT_SETUP                                                            \
     _ut_UT_SETUP(, __FILE__)
 
 /**
+ * Add a setup function to a given test suite.
+ *
+ * This code will be run before each test in the test suite with given name.
+ *
+ * @param suitename     the test suite to be extended with this code
  */
-#define UT_SUITE_TEARDOWN(suitename)                                        \
-    _ut_UT_TEARDOWN(suitename, STRINGIFY(suitename))
+#define UT_SUITE_SETUP(suitename)                                           \
+    _ut_UT_SETUP(suitename, STRINGIFY(suitename))
 
 /**
+ * Add a teardown to the default file suite.
+ *
+ * This code will be run after each test in this file that is not explicitly
+ * added to a named test suite.
  */
 #define UT_TEARDOWN                                                         \
     _ut_UT_TEARDOWN(, __FILE__)
 
+/**
+ * Add a teardown function to a given test suite.
+ *
+ * This code will be run after each test in the test suite with given name.
+ */
+#define UT_SUITE_TEARDOWN(suitename)                                        \
+    _ut_UT_TEARDOWN(suitename, STRINGIFY(suitename))
 
 /**
  * Run all defined tests.
@@ -189,12 +231,12 @@ int ut_run_all_tests(void);
  * This function is intended for writing custom assertions. Direct calls
  * are not advised.
  *
- * @param file  filename where the assertion takes place
+ * @param fname filename where the assertion takes place
  * @param line  line number where the assertion is located
  * @param expr  expression that is tested by the assertion
- * @param msg   printf-like format string to print error message in case
+ * @param msg   `printf`-like format string to print error message in case
  *              assertion fails
  */
-void _ut_assert_func(const char *file, int line, int expr, const char *msg, ...);
+void _ut_assert_func(const char *fname, int line, int expr, const char *msg, ...);
 
 #endif /* end of include guard: UTEST_H */
