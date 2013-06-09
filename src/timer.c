6bcabc63b71ce43baed4b115b427f1acd86b6e2f
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2013-06-09 14:56:49 +0200

    Track whether timer is running
    This should make it more difficult to use the module incorrectly. When
    something fishy is going on, the timer prints warnings to stderr.

--- src/timer.c
+++ src/timer.c
@@ -1,11 +1,14 @@
 #include "timer.h"
 
+#include <stdbool.h>
+#include <stdio.h>
 #include <time.h>
 
 #include <stdlib.h>
 
 struct timer {
     struct timespec total;
+    bool running;
 };
 
 static inline void
@@ -37,6 +40,7 @@ Timer * timer_new(void)
     Timer *t = malloc(sizeof *t);
     t->total.tv_sec = 0;
     t->total.tv_nsec = 0;
+    t->running = false;
     return t;
 }
 
@@ -47,19 +51,30 @@ void timer_free(Timer *t)
 
 void timer_start(Timer *timer)
 {
+    if (timer->running) {
+        fprintf(stderr, " *** WARNING: timer is already running\n");
+    }
     struct timespec tmp = { 0, 0 };
     clock_gettime(CLOCK_REALTIME, &tmp);
     time_sub(&timer->total, &tmp);
+    timer->running = true;
 }
 
 void timer_stop(Timer *timer)
 {
+    if (!timer->running) {
+        fprintf(stderr, " *** WARNING: timer is not running\n");
+    }
     struct timespec tmp = { 0, 0 };
     clock_gettime(CLOCK_REALTIME, &tmp);
     time_add(&timer->total, &tmp);
+    timer->running = false;
 }
 
 double timer_get_elapsed(Timer *timer)
 {
+    if (timer->running) {
+        fprintf(stderr, " *** WARNING: timer is running\n");
+    }
     return timer->total.tv_sec + timer->total.tv_nsec / 1000000000.0;
 }
