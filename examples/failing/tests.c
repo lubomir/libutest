4f4c991ccb723f66238ba8d58cb18ca962998475
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2013-06-16 14:52:42 +0200

    Add example with failing assertions
    This will make debugging easier as many different outputs are printed
    and can be examined.

---
+++ examples/failing/tests.c
@@ -0,0 +1,30 @@
+#include <utest.h>
+
+#include <stdio.h>
+#include <string.h>
+
+UT_TEST(pointers) {
+    int a;
+    ut_assert_null(&a);
+    ut_assert_not_null(strstr("foobar", "X"));
+}
+
+UT_TEST(integers) {
+    int a = 10;
+    ut_assert_equal_int(15, a);
+    unsigned b = 10;
+    ut_assert_equal_uint(0, b);
+}
+
+UT_TEST(crash) {
+    int *a = NULL;
+    *a = 5;
+}
+
+UT_TEST(strings) {
+    const char *str = "foobarbaz";
+    ut_assert_equal_string("fooquubaz", str);
+    ut_assert_equal_string(NULL, str);
+    str = NULL;
+    ut_assert_equal_string("foobar", str);
+}
